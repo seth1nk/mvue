4f2501d82c5af49ce59896c12a95974fc18370c3
seth1nk <kostik.vs10@mail.ru>
2025-05-25 20:58:32 +0800

    Update ViewVacancy.vue

--- src/components/ViewVacancy.vue
+++ src/components/ViewVacancy.vue
@@ -34,7 +34,7 @@ export default {
       vacancy: null,
       loading: true,
       errorMessage: null,
-      backendUrl: 'http://localhost:3000',
+      backendUrl: 'https://mnode.onrender.com',
     };
   },
   async created() {
